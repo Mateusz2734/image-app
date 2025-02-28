5c627f7a5b516964acd14e6b9acb089057c80d01
Mateusz Wala <mateusz.wala2734@gmail.com>
2025-02-28 15:07:15 +0100

    chore: Update frontend configs

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,29 +1,31 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import svgr from 'vite-plugin-svgr';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 
 // if in ESM context
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const ReactCompilerConfig = {
-  target: '18',
+  target: "18",
 };
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react({
-    babel: {
-      plugins: [
-        ['babel-plugin-react-compiler', ReactCompilerConfig],
-      ],
-    },
-  }), svgr()],
+  plugins: [
+    react({
+      babel: {
+        plugins: [["babel-plugin-react-compiler", ReactCompilerConfig]],
+      },
+    }),
+    svgr(),
+  ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
-    }
-  }
+      "@": path.resolve(__dirname, "./src"),
+      "@wails": path.resolve(__dirname, "./wailsjs"),
+    },
+  },
 });
