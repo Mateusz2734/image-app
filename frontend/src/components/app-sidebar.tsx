878abb03ea633a384e33f7a97e96a21d32eb7dfe
Mateusz Wala <mateusz.wala2734@gmail.com>
2025-03-10 23:46:49 +0100

    feat: Add tooltip functionality to sidebar links

--- frontend/src/components/app-sidebar.tsx
+++ frontend/src/components/app-sidebar.tsx
@@ -3,6 +3,7 @@ import { Link } from "react-router";
 import { FaCompressArrowsAlt, FaFileImage } from "react-icons/fa";
 
 import { Sidebar, SidebarMenu, SidebarMenuButton } from "@/components/ui/sidebar";
+import { TooltipProvider, TooltipTrigger, Tooltip, TooltipContent } from "@/components/ui/tooltip";
 
 
 export default function AppSidebar() {
@@ -14,19 +15,35 @@ export default function AppSidebar() {
 
     return (
         <Sidebar collapsible="none">
-            <SidebarMenu className="h-screen flex flex-col items-center justify-center">
-                <Link to="/" onClick={() => handleClick("home")}>
-                    <SidebarMenuButton asChild isActive={active === "home"} aria-label="Change Type">
-                        <FaFileImage />
-                    </SidebarMenuButton>
-                </Link>
+            <TooltipProvider>
+                <SidebarMenu className="h-screen flex flex-col items-center justify-center">
+                    <Tooltip>
+                        <TooltipTrigger asChild>
+                            <Link to="/" onClick={() => handleClick("home")}>
+                                <SidebarMenuButton asChild isActive={active === "home"} aria-label="Change Type">
+                                    <FaFileImage />
+                                </SidebarMenuButton>
+                            </Link>
+                        </TooltipTrigger>
+                        <TooltipContent>
+                            Convert images
+                        </TooltipContent>
+                    </Tooltip>
 
-                <Link to="/compress" onClick={() => handleClick("compress")}>
-                    <SidebarMenuButton asChild isActive={active === "compress"} aria-label="Compress Images">
-                        <FaCompressArrowsAlt />
-                    </SidebarMenuButton>
-                </Link>
-            </SidebarMenu>
+                    <Tooltip>
+                        <TooltipTrigger asChild>
+                            <Link to="/compress" onClick={() => handleClick("compress")}>
+                                <SidebarMenuButton asChild isActive={active === "compress"} aria-label="Compress Images">
+                                    <FaCompressArrowsAlt />
+                                </SidebarMenuButton>
+                            </Link>
+                        </TooltipTrigger>
+                        <TooltipContent>
+                            Compress images
+                        </TooltipContent>
+                    </Tooltip>
+                </SidebarMenu>
+            </TooltipProvider>
         </Sidebar>
     );
 }
